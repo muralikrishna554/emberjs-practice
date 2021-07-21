9811e234b0d5d0a34ecba39c28ee07dba5cac269
kalluri <mk.kalluri231@gmail.com>
2021-07-21 13:02:39 +0530

    local to reflect in github

--- app/router.js
+++ app/router.js
@@ -5,9 +5,7 @@ export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
-
-Router.map(function () {
+Router.map(function) ({
   this.route('about');
-  this.route('contact', { path: '/getting-in-touch' });
-
+  this.route('contact', { path: '' });
 });
